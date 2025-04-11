837632d53fc648c6528e09ff2bb1b01d4ff2e61f
root <root@logs.edw.ee>
2025-04-11 15:35:49 +0000

    modified:   auxiliary/load_replica_history_csv.sh

--- auxiliary/load_replica_history_csv.sh
+++ auxiliary/load_replica_history_csv.sh
@@ -23,6 +23,8 @@ CREATE TABLE IF NOT EXISTS replica_history (
     gtid_io_pos VARCHAR(200) NOT NULL,
     slave_sql_running_state VARCHAR(500),
     handler_read_rnd_next bigint,
+    relay_log_file VARCHAR(200) DEFAULT NULL,
+    relay_log_pos VARCHAR(200) DEFAULT NULL,
     PRIMARY KEY (id)
 )
 COLLATE='utf8mb4_general_ci' ENGINE=InnoDB;
